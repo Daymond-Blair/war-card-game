fd1482d51f82e2d4de716db58029600a6afe505b
Mondday <dblair1987@live.com>
2019-02-26 00:08:06 -0800

    Updating comments

--- oop_project.py
+++ oop_project.py
@@ -1,34 +1,6 @@
-#####################################
-### WELCOME TO YOUR OOP PROJECT #####
-#####################################
-
-# For this project you will be using OOP to create a card game. This card game will
-# be the card game "War" for two players, you an the computer. If you don't know
-# how to play "War" here are the basic rules:
-#
-# The deck is divided evenly, with each player receiving 26 cards, dealt one at a time,
-# face down. Anyone may deal first. Each player places his stack of cards face down,
-# in front of him.
-#
-# The Play:
-#
-# Each player turns up a card at the same time and the player with the higher card
-# takes both cards and puts them, face down, on the bottom of his stack.
-#
-# If the cards are the same rank, it is War. Each player turns up three cards face
-# down and one card face up. The player with the higher cards takes both piles
-# (six cards). If the turned-up cards are again the same rank, each player places
-# another card face down and turns another card face up. The player with the
-# higher card takes all 10 cards, and so on.
-#
-# There are some more variations on this but we will keep it simple for now.
-# Ignore "double" wars
-#
-# https://en.wikipedia.org/wiki/War_(card_game)
-
 from random import shuffle
 
-# Two useful variables for creating Cards.
+# create cards
 SUITE = 'H D S C'.split()
 RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
 
@@ -110,8 +82,6 @@ class Player:
 #### GAME PLAY #######
 ######################
 print("Welcome to War, let's begin...")
-
-# Use the 3 classes along with some logic to play a game of war!
 
 #[x] instantiate deck
 #[x] instantiate player one object from player
